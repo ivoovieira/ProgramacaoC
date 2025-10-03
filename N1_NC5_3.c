ce84b49b43b3500a8f5c8dee596a676f7516abf1
ivoovieira <202509233375@alunos.estacio.br>
2025-10-03 17:52:53 -0300

    Create N1_NC5_3.c

---
+++ N1_NC5_3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int main() {
+    float idade;
+    char opcao;
+    char nome[20];
+    // sintaxe scanf
+    // scanf"("formato", "formato2", &variavel1, &variavel2,...)
+    
+    printf("Digite sua idade: ");
+    scanf("%d", &idade);
+    printf("A idade é: %d", idade);
+    printf
+
+
+
+}
