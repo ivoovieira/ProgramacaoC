5aeda0d3b572376e9fe2aa749e6929b55f72cd83
ivoovieira <202509233375@alunos.estacio.br>
2025-10-05 09:04:37 -0300

    N1_NC5_2.c

--- N1_NC5_2.c
+++ N1_NC5_2.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
-
-int main (){
-    int idade;
-    int quantidade = 1;
+ 
+int main() {
+    int idade = 25;
     float altura = 1.75;
-    double peso = 90.3; 
-    char letra = 'A';
-    char nome[20] = 'sergio';
-   
-    idade = 25;
-    quantidade  = 10;
-    
-
-
+    char opcao = 'S';
+    char nome[20] = "Sergio";
+ 
+   //printf("A idade do %s é: %d\n", nome, idade);
+    printf("A idade é: %d\n", idade);
+    printf("O nome é: %s\n", nome);
+    printf("A altura é: %.2f\n", altura);
+    printf("A opção é: %c\n", opcao);
+ 
+    return 0;
 }
+/*
+prinf("%formato1 %formato2 %formato3, variavel1,variavel2,variavel3")
+
+%d: Imprime um inteiro no formato decimal
+%i: Equivale a %d
+%f: Imprime um número de ponto flutuante (float) no formato padrão
+%e: Imprime um número de ponto flutuante no formato exponencial
+%c: Imprime um caractere
+%s: Imprime uma string (sequência de caracteres)
+*/
